e66b1ad1082764657e34cb05b0469f6fe05f5526
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-30 00:11:44 +0100

    Redirect to error page on failure

--- src/main/java/com/codecool/dadsinventory/security/config/SecurityConfig.java
+++ src/main/java/com/codecool/dadsinventory/security/config/SecurityConfig.java
@@ -5,11 +5,8 @@ import com.codecool.dadsinventory.security.filter.AuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -46,10 +43,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/home/privacy**").hasRole("MUM")
                 .antMatchers("/*").hasRole("DAD")
                 .antMatchers("/login*").permitAll()
+                .antMatchers("/error").permitAll()
                 .antMatchers("/api/v1/users**").permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new AuthenticationFilter(authenticationManagerBean()))
                 .addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.formLogin();
+        http.formLogin().failureUrl("/error");
     }
 }
