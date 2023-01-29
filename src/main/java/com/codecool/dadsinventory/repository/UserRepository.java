58b7dced0b1fd7320777ce416f2bc3b41fc24758
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-29 16:43:32 +0100

    Fix missing annotation on UserRepository

--- src/main/java/com/codecool/dadsinventory/repository/UserRepository.java
+++ src/main/java/com/codecool/dadsinventory/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.codecool.dadsinventory.repository;
 
 import com.codecool.dadsinventory.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Long, UserEntity> {
 }
 
