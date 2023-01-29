ab965b2ff128a85d68a6ac1b744a6447c37fd6fb
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-01-29 18:07:44 +0100

    Implement AuthenticationFilter

---
+++ src/main/java/com/codecool/dadsinventory/security/filter/AuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.codecool.dadsinventory.security.filter;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import lombok.extern.slf4j.Slf4j;
+import com.auth0.jwt.JWT;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+        return authenticationManager.authenticate(token);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        User user = (User) authResult.getPrincipal();
+        log.info("{} has {} role",user.getUsername(), user.getAuthorities());
+        Algorithm algorithm = Algorithm.HMAC256("D4d5|nv3nt0ry");
+        String token = JWT.create()
+                .withSubject(user.getUsername())
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("role",user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+        Cookie cookie = new Cookie("token",token);
+        cookie.setMaxAge(60*10*10);
+        cookie.setSecure(true);
+        response.addCookie(cookie);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.sendRedirect("/index");
+    }
+}
